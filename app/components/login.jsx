4a3ec7089730f47f74251ccce2c9f0dfc96479ea
dogunfx <dogunfx@gmail.com>
2024-06-19 13:40:54 +0100

    completed login

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -1,7 +1,22 @@
+"use client";
+import { Input, Card, Button, Form } from "antd";
 export default function Login() {
+  const [myLoginForm] = Form.useForm();
+
   return (
-    <>
-      <input type="text" />
-    </>
+    <Card title="My Login Form" className=" mx-auto my-52  sm:w-1/2">
+      <h1 className="mb-3 text-2xl  sm:text-3xl">
+        Please Enter your Details Below
+      </h1>
+      <Form form={myLoginForm}>
+        <Form.Item name="email">
+          <Input placeholder="Enter Email" />
+        </Form.Item>
+        <Form.Item name="password">
+          <Input.Password placeholder="Enter Password" />
+        </Form.Item>
+      </Form>
+      <Button type="primary">Login</Button>
+    </Card>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,6 @@ import Login from "./components/login";
 export default function Home() {
   return (
     <main>
-      <h2>My Login Form</h2>
       <Login />
     </main>
   );
