d1ccd70d5b329e7f30e6a5ff00bca1676df68107
dogunfx <dogunfx@gmail.com>
2024-06-20 13:07:16 +0100

    completed quiz

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -1,7 +1,22 @@
 "use client";
-import { Input, Card, Button, Form } from "antd";
+import { Input, Card, Button, Form, message } from "antd";
+import { useRouter } from "next/navigation";
 export default function Login() {
   const [myLoginForm] = Form.useForm();
+  let router = useRouter();
+
+  async function handleLogin() {
+    await myLoginForm.validateFields();
+    let email = myLoginForm.getFieldValue("email");
+    let password = myLoginForm.getFieldValue("password");
+
+    if (email == "dogunfx@gmail.com" && password == "12345") {
+      message.success("Login successful");
+      router.push("/dashboard");
+    } else {
+      message.error("Login Failed");
+    }
+  }
 
   return (
     <Card title="My Login Form" className=" mx-auto my-52  sm:w-1/2">
@@ -9,14 +24,16 @@ export default function Login() {
         Please Enter your Details Below
       </h1>
       <Form form={myLoginForm}>
-        <Form.Item name="email">
+        <Form.Item name="email" rules={[{ required: true }]}>
           <Input placeholder="Enter Email" />
         </Form.Item>
-        <Form.Item name="password">
+        <Form.Item name="password" rules={[{ required: true }]}>
           <Input.Password placeholder="Enter Password" />
         </Form.Item>
       </Form>
-      <Button type="primary">Login</Button>
+      <Button type="primary" onClick={handleLogin}>
+        Login
+      </Button>
     </Card>
   );
 }
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { Card, Button, Form, Input, message } from "antd";
+import { useState } from "react";
+
+export default function Dashboard() {
+  const [myFormState] = Form.useForm();
+
+  let [fn, updateFn] = useState(5);
+  let [sn, updateSn] = useState(19);
+  let [score, updateScore] = useState(0);
+  let [total, updateTotal] = useState(1);
+
+  function checkAnswer() {
+    let ans = myFormState.getFieldValue("answer");
+
+    if (ans == fn * sn) {
+      message.success("Wow!!😳 CorrectCorrect ✅✅✅");
+      let newScore = score + 1;
+      updateScore(newScore);
+    } else {
+      message.error(
+        "OLODO,😂😂😂 --- You are wrong the correct answer is " + fn * sn
+      );
+    }
+    changeQuestion();
+  }
+
+  function changeQuestion() {
+    updateFn(Math.ceil(Math.random() * 20));
+    updateSn(Math.ceil(Math.random() * 15));
+    let newTotal = total + 1;
+    updateTotal(newTotal);
+  }
+  return (
+    <Card
+      title="Welcome to your Math quiz..."
+      className="sm:w-1/2 mx-auto my-60"
+    >
+      <div className="text-center">
+        <h3 className="text-2xl ">Welcome DogunFX,</h3>
+        <p>
+          Your Current score is: {score} out of : {total}
+        </p>
+        <br />
+        <p className="text-4xl text-purple-800 ">
+          What is the answer to {fn} * {sn} ?
+        </p>
+        <br />
+
+        <Form form={myFormState}>
+          <Form.Item name="answer">
+            <Input placeholder="Enter the Answer here" />
+          </Form.Item>
+        </Form>
+      </div>
+      <div className="flex">
+        <Button type="primary" className="mr-auto" onClick={changeQuestion}>
+          Skip This
+        </Button>
+        <Button onClick={checkAnswer}>Check Answer</Button>
+      </div>
+    </Card>
+  );
+}
